476e16d2fa9d72883e545e6c82f430cb399ae813
The MUDO Group <hello@mudogroup.com>
2017-06-19 12:28:52 +0100

    Create update-contact.php

---
+++ update-contact.php
@@ -0,0 +1,36 @@
+<?
+
+    $api_key = "YOUR_API_KEY";
+    $url = "https://mudogroup.com/api/v1/contacts/update";
+
+    $post = [
+        'account_id' => 'ACCOUNT_ID',
+        'account_name' => 'NEW ACCOUNT NAME'
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey:'.$api_key));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    $response = curl_exec($ch);
+    $info = curl_getinfo($ch);
+
+    if($info['http_code'] == 200) {
+        
+        $response = json_decode($response);
+        echo 'Contact updated successfully!';
+        
+    } elseif($info['http_code'] == 403) {
+        
+        echo 'API Key Not Found!';
+        
+    } elseif($info['http_code'] == 404) {
+        
+      echo 'Please check your URL';
+        
+    }
+
+    echo '<pre>'; print_r($response); echo '</pre>';
+
+    curl_close($ch);
+?>
